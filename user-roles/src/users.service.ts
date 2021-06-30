943561c8723d46269454b2a281a5727e1d1f3d8b
Konrad Polek <konriz@gmail.com>
2021-06-30 05:41:02 +0200

    Create users.service.ts

---
+++ user-roles/src/users.service.ts
@@ -0,0 +1,25 @@
+ import { Injectable } from '@nestjs/common';
+ import { User } from './user.entity';
+
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+      roles: [Role.Admin]
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+      roles: [Role.User]
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username);
+  }
+}
